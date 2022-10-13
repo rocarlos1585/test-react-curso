95c9ebd872ba5f14c3ce70517fab8eb0d5b69773
Ricardo Bustos <za19012330@zapopan.tecmm.edu.mx>
2022-10-12 23:30:50 -0500

    create change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import InputForm from './Componets/inputForm';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='App'>
+       <form action="">
+   <strong class="title">Matter</strong>   
+      <InputForm/>
+    <h4> Degree </h4>
+  <input placeholder="Degree"/>
+  <br/>
+  </form>
     </div>
-  );
+  )
 }
 
 export default App;
+
---
+++ src/formularioMateria/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils'
+
+
+const InputForm = () => {
+    return (
+      <div>
+          <h4>Subject Name</h4>
+    <input placeholder="Subject Name"/>
+      </div>
+      
+    )
+  
+  
+  }
+  
+  
+  
+  
+  export default InputForm 
