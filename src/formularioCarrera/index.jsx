8f6fd79db222bc71667386f6177f0e90bae142ce
Gerry Espinoza Sánchez <113739342+GerryEspinoza126@users.noreply.github.com>
2022-10-12 22:47:19 -0500

    Adding the last details

--- src/formularioCarrera/index.jsx
+++ src/formularioCarrera/index.jsx
@@ -9,6 +9,7 @@ const Index = () => {
         duracion: 0,
       });
     return <div className="container-carrera">
+        <h2>Formulario de carrera</h2>
         <Input label="Nombre" type="text" placeholder="Nombre de la carrera" setData={setDataForm} data={dataForm} />
         <Input label="Duracion" type="number" placeholder="Duracion carrera" setData={setDataForm} data={dataForm} />
         <Button text="Enviar" data={dataForm} />
